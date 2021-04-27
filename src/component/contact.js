a0af9d9319d25284fd12d8e9fa5a0f71052c71f0
NaveenBadhan <naveenbadhan27@gmail.com>
2021-04-27 11:34:35 +0530

    Create contact.js

---
+++ src/component/contact.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+const Contact = () => {
+    return(
+        <div className="body">
+            Hi,Contact
+        </div>
+    )
+}
+
+
+export default Contact;
