f8420c20c46d86553977df35c5388690e169a89b
NaveenBadhan <naveenbadhan27@gmail.com>
2021-04-27 11:34:32 +0530

    Create about.js

---
+++ src/component/about.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+const About = () => {
+    return(
+        <div className="body">
+            Hi,About
+        </div>
+    )
+}
+
+
+export default About;
