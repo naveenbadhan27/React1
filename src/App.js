24727e8a9df0583db8f60afccac62d8f859d69a3
NaveenBadhan <naveenbadhan27@gmail.com>
2021-04-28 11:10:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Home from './component/home';
 import About from './component/about';
 import Contact from './component/contact';
+import Login from './component/login';
 import Nav from './component/nav';
 function App() {
   return (
